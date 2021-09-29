4123edffa737d935a702fc0305ee1e44a0a2b3d4
Joesh Gichon <joeshgichon56@gmail.com>
2021-09-29 09:24:56 +0300

    updated my models with tables

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 from . import db
 from datetime import datetime
-from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
 
@@ -14,3 +13,62 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.name}'
+
+class UserPayment(db.Model):
+
+    __tablename__ = 'payments'
+
+    payment_id=db.Column(db.Integer,primary_key=True)
+    payment_type=db.Column(db.String(length=50),nullable=False)
+    account_no=db.Column(db.Integer,nullable=False)
+
+    def __repr__(self):
+        return f'UserPayment {self.account_no}-{self.payment_type}'
+
+class Product(db.Model):
+
+    __tablename__="products"
+
+    product_id=db.Column(db.Integer,primary_key=True,nullable=False)
+    product_name=db.Column(db.String(length=30),nullable=False,unique=True)
+    price=db.Column(db.Integer,nullable=False)
+    desc=db.Column(db.String(length=1024),nullable=False,unique=True)
+
+    def __repr__(self):
+        return f'Product {self.name}-{self.price}'
+
+class ProductCategory(db.Model):
+
+    __tablename__="categories"
+
+    category_id=db.Column(db.Integer,primary_key=True,nullable=False)
+    category_name=db.Column(db.String(length=100),nullable=False)
+    category_desc=db.Column(db.String(length=1025),nullable=False)
+
+class Order(db.Model):
+
+    __tablename__="orders"
+
+    order_id=db.Column(db.Integer,primary_key=True,nullable=False)
+    status=db.Column(db.String(length=1025))
+    created_at=db.Column(db.DateTime, default=datetime)
+
+    user_id=db.Column(db.Integer,db.ForeignKey("user_id"))
+
+    def __repr__(self):
+        return f"{self.status}"
+
+class OrderItems(db.Model):
+
+    __tablename__="items"
+
+    item_id=db.Column(db.Integer,primary_key=True)
+    quantity = db.Column(db.Integer)
+
+    order_id=db.Column(db.Integer,db.ForeignKey("order_id"))
+    product_id=db.Column(db.Integer,db.ForeignKey("product_id"))
+    
+    def __repr__(self):
+        return f"{self.quantity}"
+
+
